62da23429c74e01a4a0e3a667f589792f8ecdc57
Timothy Reddy <timothy.reddy@climate.com>
2016-04-20 08:03:30 -0500

    Initial project commit

---
+++ sandbox/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  sandbox
+//
+//  Created by Timothy Reddy on 4/20/16.
+//  Copyright © 2016 Timothy Reddy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ sandbox/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  sandbox
+//
+//  Created by Timothy Reddy on 4/20/16.
+//  Copyright © 2016 Timothy Reddy. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
