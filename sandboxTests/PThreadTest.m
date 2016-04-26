73348c53e8929544df2d59a7f2d48ffabab8c86c
Timothy Reddy <timothy.reddy@climate.com>
2016-04-26 10:21:18 -0500

    more pthread stuff

--- sandboxTests/PThreadTest.m
+++ sandboxTests/PThreadTest.m
@@ -9,6 +9,8 @@
 #import <XCTest/XCTest.h>
 #import <pthread.h>
 
+static pthread_mutex_t mutex_ = PTHREAD_MUTEX_INITIALIZER;
+
 @interface PThreadTest : XCTestCase {
     pthread_rwlock_t _rwlock;
     int _value;
@@ -48,8 +50,34 @@
     });
     
     [self waitForExpectationsWithTimeout:6.0 handler:nil];
-
 }
+
+-(void)testMutex {
+    dispatch_queue_t workQueue = dispatch_queue_create("com.work", DISPATCH_QUEUE_CONCURRENT);
+    _value = 0;
+    for(int i = 0; i < 1000; i++) {
+        dispatch_async(workQueue, ^{
+            if(arc4random() % 4 == 0) {
+                pthread_mutex_lock(&mutex_);
+                _value++;
+                NSLog(@"[%d] write:%d", i, _value);
+                pthread_mutex_unlock(&mutex_);
+            } else {
+                pthread_mutex_lock(&mutex_);
+                NSLog(@"[%d] read:%d", i, _value);
+                pthread_mutex_unlock(&mutex_);
+            }
+        });
+    }
+    XCTestExpectation* expectation = [self expectationWithDescription:@"dude"];
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        [expectation fulfill];
+    });
+    
+    [self waitForExpectationsWithTimeout:6.0 handler:nil];
+}
+
+#pragma mark Private Methods
 
 -(void)read:(int)i {
     pthread_rwlock_rdlock(&_rwlock);
